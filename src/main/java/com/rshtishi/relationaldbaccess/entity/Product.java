c44b488ef46a2e99a5e51bf1c4f338ad3f319a79
rshtishi <rando.shtishi@@gmail.com>
2020-08-30 14:42:25 +0200

    transforming in a bidirectional one to one association

--- src/main/java/com/rshtishi/relationaldbaccess/entity/Product.java
+++ src/main/java/com/rshtishi/relationaldbaccess/entity/Product.java
@@ -9,6 +9,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -26,18 +27,26 @@ public class Product {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	@NonNull private String name;
+	@NonNull
+	private String name;
 	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
-	List<Review> reviews= new ArrayList<>();
-	
+	private List<Review> reviews = new ArrayList<>();
+	@OneToOne(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+	private ProductDetails details;
+
 	public void addReview(Review review) {
 		reviews.add(review);
 		review.setProduct(this);
 	}
-	
+
 	public void removeReview(Review review) {
 		reviews.remove(review);
 		review.setProduct(null);
+	}
+	
+	public void setDetails(ProductDetails details) {
+		this.details = details;
+		details.setProduct(this);
 	}
 
 }
