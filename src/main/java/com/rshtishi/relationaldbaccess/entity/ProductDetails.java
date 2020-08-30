cdfb42b16542c6ae25781b18a8110c8cc630c1f7
rshtishi <rando.shtishi@@gmail.com>
2020-08-30 12:26:24 +0200

    mapping the relationship betwen product and product details

---
+++ src/main/java/com/rshtishi/relationaldbaccess/entity/ProductDetails.java
@@ -0,0 +1,33 @@
+package com.rshtishi.relationaldbaccess.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class ProductDetails {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@NonNull private String createdBy;
+	@NonNull private String createdOn;
+	@OneToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="product_id")
+	private Product product;
+
+}
